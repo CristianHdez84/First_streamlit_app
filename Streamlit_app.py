eb08031a8a04938f4b8e8ddc34ef8d37cf62de17
Cristian Hernandez <119022795+CristianHdez84@users.noreply.github.com>
2022-11-24 21:37:02 -0500

    Update Streamlit_app.py
    Added header
    Added 3 captions

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.captio('Omega 3 & Blueberry Oatmeal')
+streamlit.captio('Kale, Spinach & Rocket Smoothie')
+streamlit.captio('Hard-Boiled Free-Range Egg')
